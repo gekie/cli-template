e6a10c9b92a4d02414641c427b01041c0a134a1c
ChenJhai <gekie@163.com>
2018-10-11 13:53:33 +0800

    add new Command

--- src/main/java/com/xjj/tools/bigdata/tunnel/Test.java
+++ src/main/java/com/xjj/tools/bigdata/tunnel/Test.java
@@ -1,12 +1,20 @@
 package com.xjj.tools.bigdata.tunnel;
 
+import org.fusesource.jansi.Ansi;
 import org.fusesource.jansi.AnsiConsole;
 
 public class Test {
     public static void main(String[] args) throws Exception{
         AnsiConsole.systemInstall();
-        System.out.println("dskflsaljdkfjalsdf");
-        System.out.print("\u001B[2A\u001B[15C OK");
-        System.out.println("FFF");
+
+        int i=1;
+        while(true){
+            System.out.print(Ansi.ansi(20).bold().cursorToColumn(10).fgYellow().a(i).fgRed().a("/100"));
+            if(i>=100){
+                break;
+            }
+            i++;
+            Thread.sleep(1000);
+        }
     }
 }
